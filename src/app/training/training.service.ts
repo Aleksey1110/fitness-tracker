882e53e76337b8eced44926ff0c72b411590302b
Alex Posysoev <alekseyposysoev@gmail.com>
2019-02-25 13:25:11 +0300

    training.service

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -17,15 +17,16 @@ export class TrainingService {
 
     fetchAvailableExercises() {
         this.db
-            .collection('availiableExercises')
+            .collection('avaliableExercises')
             .snapshotChanges()
             .pipe(map(docArray => {
                 return docArray.map(doc => {
                     return {
                         id: doc.payload.doc.id,
-                        name: doc.payload.doc.data().name,
-                        duration: doc.payload.doc.data().duration,
-                        calories: doc.payload.doc.data().calories
+                        // name: doc.payload.doc.data().name,
+                        // duration: doc.payload.doc.data().duration,
+                        // calories: doc.payload.doc.data().calories
+                        ...doc.payload.doc.data()
                     };
                 });
             }))
